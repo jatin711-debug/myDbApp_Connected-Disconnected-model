a2fd8d171ff87675f25d8a00d08dc519d49a635d
jatin711-debug <69040077+jatin711-debug@users.noreply.github.com>
2021-12-02 15:17:03 -0500

    addin crud oprations

---
+++ ConsoleApp1/CrudOperation.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace myDbApp
+{
+    class CrudOperation
+    {
+        private SqlConnection sqlConnection;
+        private SqlDataAdapter sqlDataAdapter;
+        private SqlCommandBuilder sqlCommandBuilder;
+        private DataSet dataSet;
+        private DataTable dataTable;
+
+        internal CrudOperation()
+        {
+            string cs = getConnectionString("db");
+            string query = "Select ProductID,ProductName,UnitPrice,UnitsInStock from Products";
+            sqlConnection = new SqlConnection(cs);
+            sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+            sqlCommandBuilder = new SqlCommandBuilder(sqlDataAdapter);
+            dataSet = new DataSet();
+            FillDataSet();
+        }
+
+        private void FillDataSet()
+        {
+            dataSet = new DataSet();
+            sqlDataAdapter.Fill(dataSet, "Products");
+            dataTable = dataSet.Tables["Products"];
+        }
+
+        internal void GetAllProducts()
+        {
+            foreach(DataRow dataRow in dataTable.Rows)
+            {
+                Console.WriteLine($"{dataRow["ProductID"]}  {dataRow["ProductName"]}");
+            }
+        }
+
+        internal void GetProductById(int id)
+        {
+            DataRow dataRow = dataTable.Rows.Find(id);
+            if(dataRow != null) Console.WriteLine($"{dataRow["ProductID"]}  {dataRow["ProductName"]}");
+            else
+            {
+                Console.WriteLine("Invalid Product ID");
+            }
+        }
+
+        internal void InsertProduct(string name,int price,int quantity)
+        {
+            DataRow row = dataTable.NewRow();
+            row["ProductID"] = 0;
+            row["ProductName"] = name;
+            row["UnitPrice"] = price;
+            row["UnitsInStock"] = quantity;
+            dataTable.Rows.Add(row);
+            sqlDataAdapter.InsertCommand = sqlCommandBuilder.GetInsertCommand();
+            sqlDataAdapter.Update(dataTable);
+            FillDataSet();
+        }
+
+        internal void UpdateProduct(int id,string name, int price, int quantity)
+        {
+            DataRow row = dataTable.Rows.Find(id);
+            row["ProductName"] = name;
+            row["UnitPrice"] = price;
+            row["UnitsInStock"] = quantity;
+            sqlDataAdapter.UpdateCommand = sqlCommandBuilder.GetUpdateCommand();
+            sqlDataAdapter.Update(dataTable);
+            FillDataSet();
+        }
+
+        internal void DeleteProduct(int id)
+        {
+            DataRow row = dataTable.Rows.Find(id);
+            row.Delete();
+            sqlDataAdapter.DeleteCommand = sqlCommandBuilder.GetDeleteCommand();
+            sqlDataAdapter.Update(dataTable);
+            FillDataSet();
+        }
+
+        private string getConnectionString(string name)
+        {
+            ConfigurationBuilder builer = new ConfigurationBuilder();
+            builer.SetBasePath(Directory.GetCurrentDirectory());
+            builer.AddJsonFile("config.json");
+            IConfiguration conf = builer.Build();
+            return conf["ConnectionStrings:" + name];
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using System.IO;
 using System.Data.SqlClient;
 using System.Data;
+using myDbApp;
 
 namespace ConsoleApp1
 {
@@ -10,10 +11,90 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            PrintProducts();
-            //countTable();
+            CrudOperation crud = new CrudOperation();
+
+            do
+            {
+                int choice = DisplayMenu();
+
+                switch (choice)
+                {
+                    case 1: // Get All Products
+                        crud.GetAllProducts();
+                        break;
+
+                    case 2: // Get Product by ID
+                        Console.Write("\nEnter Product ID: ");
+                        int id = int.Parse(Console.ReadLine());
+                        crud.GetProductById(id);
+                        break;
+
+                    case 3: // Insert Product
+                        Console.Write("\nEnter Product Name: ");
+                        string name = Console.ReadLine();
+
+                        Console.Write("Enter Product Price: ");
+                        int price = int.Parse(Console.ReadLine());
+
+                        Console.Write("Enter Product Quantity: ");
+                        short quantity = short.Parse(Console.ReadLine());
+
+                        crud.InsertProduct(name, price, quantity);
+                        break;
+
+                    case 4: // Update Product
+                        Console.Write("\nEnter Product Id: ");
+                        id = int.Parse(Console.ReadLine());
+                        crud.GetProductById(id);
+
+                        Console.Write("\nEnter Product Name: ");
+                        name = Console.ReadLine();
+
+                        Console.Write("Enter Product Price: ");
+                        price = int.Parse(Console.ReadLine());
+
+                        Console.Write("Enter Product Quantity: ");
+                        quantity = short.Parse(Console.ReadLine());
+
+                        crud.UpdateProduct(id, name, price, quantity);
+                        break;
+
+                    case 5: // Delete Product
+                        Console.Write("\nEnter Product Id: ");
+                        id = int.Parse(Console.ReadLine());
+
+                        crud.DeleteProduct(id);
+                        break;
+
+                    case 6: // Exit
+                        Environment.Exit(0);
+                        break;
+                }
+            } while (true);
         }
 
+    static int DisplayMenu()
+    {
+        Console.WriteLine("\n\n+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n");
+        Console.WriteLine("\t1 - Get all Products");
+        Console.WriteLine("\t2 - Get Product by ID");
+        Console.WriteLine("\t3 - Insert Product");
+        Console.WriteLine("\t4 - Update Product");
+        Console.WriteLine("\t5 - Delete Product");
+        Console.WriteLine("\t6 - Exit");
+
+        Console.Write("\nEnter your choice: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static string getConnectionString(string name)
+        {
+            ConfigurationBuilder builer = new ConfigurationBuilder();
+            builer.SetBasePath(Directory.GetCurrentDirectory());
+            builer.AddJsonFile("config.json");
+            IConfiguration conf = builer.Build();
+            return conf["ConnectionStrings:" + name];
+        }
         static void PrintProducts()
         {
             string cs = getConnectionString("db");
@@ -29,7 +110,7 @@ namespace ConsoleApp1
                 Console.WriteLine($"{data["ProductID"]} {data["ProductName"]}");
             }
         }
-         static void countTable()
+        static void countTable()
         {
             string cs = getConnectionString("db");
             string query = "Select Count(*) from Employees";
@@ -43,7 +124,6 @@ namespace ConsoleApp1
                 Console.WriteLine(val);
             }
         }
-
         static void printEmp()
         {
             string cs = getConnectionString("StandloneSQLServer");
@@ -62,7 +142,6 @@ namespace ConsoleApp1
                 }
             }
         }
-
         static void GetEmp()
         {
             string cs = getConnectionString("StandloneSQLServer");
@@ -82,13 +161,5 @@ namespace ConsoleApp1
             }
         }
 
-        static string getConnectionString(string name)
-        {
-            ConfigurationBuilder builer = new ConfigurationBuilder();
-            builer.SetBasePath(Directory.GetCurrentDirectory());
-            builer.AddJsonFile("config.json");
-            IConfiguration conf = builer.Build();
-            return conf["ConnectionStrings:" + name];
-        }
     }
 }
